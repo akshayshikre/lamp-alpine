9a539784bd67782cf4286c999a2c793226271946
akshayshikre <akshayshikre@gmail.com>
2018-08-13 20:34:49 +0530

    new files

---
+++ development-test.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e # terminate on errors
+set -x
+set -xo pipefail
+
+echo Testing script for developmet branch
+sleep 30
+docker images
+docker ps -a
+docker-compose logs
+docker-compose port lamp-alpine 80
+docker-compose exec lamp-alpine curl --retry 10 --retry-delay 5 -v http://127.0.0.1:80
